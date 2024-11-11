35071fd9c8f310439c86406fbe386d99fcba18cc
Lars Hædersdal <lh@navitech.dk>
2024-11-11 13:58:21 +0100

    Init

---
+++ jul.php
@@ -0,0 +1,5 @@
+<?php
+// Redirect to an external URL
+header("Location: https://tikkio.com/tickets/51143-julekoncert");
+exit();
+?>
---
+++ kalenderv1.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>Event Page</title>
+</head>
+    <div class="text-center my-4">
+        <img src="/img/ltbb.png" class="img-fluid" alt="Lyngby-Taarbæk Brass Band Logo">
+    </div>
+    <div class="container my-5">
+        <h1 class="text-center mb-4">Koncerter</h1>
+        <div class="row mb-5">
+            <div class="col-md-6">
+                <img src="/img/jul-2024.png" class="img-fluid" alt="Julekoncert Event">
+            </div>
+            <div class="col-md-6 d-flex flex-column justify-content-center">
+                <h2>Julekoncert</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas odio, vitae scelerisque enim ligula venenatis dolor.</p>
+            </div>
+        </div>
+        <div class="row mb-5">
+            <div class="col-md-6">
+                <img src="/img/nytaar-2025.png" class="img-fluid" alt="Nytårsgalla Event">
+            </div>
+            <div class="col-md-6 d-flex flex-column justify-content-center">
+                <h2>Nytårsgalla 2025</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas odio, vitae scelerisque enim ligula venenatis dolor.</p>
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
