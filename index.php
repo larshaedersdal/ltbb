468e0943ae2dffe4be17096ee0398d07505598ac
Lars Hædersdal <lh@navitech.dk>
2024-11-15 11:53:06 +0100

    galla

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+if ($_SERVER['REQUEST_URI'] === '/jul') {
+    header('Location: https://tikkio.com/tickets/51143-julekoncert');
+    exit;
+}
---
+++ nytaarsgalla.php
@@ -0,0 +1,5 @@
+<?php
+// Redirect to an external URL
+header("Location: https://tikkio.com/tickets/51226-nytarsgalla-2025");
+exit();
+?>
